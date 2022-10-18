e81f83d9119049b691e341a383f2894ade945091
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-18 15:21:22 +0200

    Fixing lint issues to make CI to run smoothly

--- R/performance_tests.R
+++ R/performance_tests.R
@@ -1,7 +1,9 @@
 #' @title Execute performance tests for a list of commits
 #'
-#' @param commit_list A list of commit hash codes, branches' names or anything else you can use with git checkout [...]
-#' @param cypress_file The path to the .js file containing cypress tests to be recorded
+#' @param commit_list A list of commit hash codes, branches' names or anything
+#' else you can use with git checkout [...]
+#' @param cypress_file The path to the .js file containing cypress tests to
+#' be recorded
 #' @param shinytest2_dir The directory with tests recorded by shinytest2
 #' @param app_dir The path to the application root
 #' @param port Port to run the app
@@ -19,10 +21,10 @@ performance_tests <- function(
     debug = FALSE
 ) {
   # Test whether we have everything we need
-  if (is.null(cypress_file) & is.null(shinytest2_dir))
+  if (is.null(cypress_file) && is.null(shinytest2_dir))
     stop("You must provide a cypress_file or the shinytest2_dir")
 
-  if (!is.null(cypress_file) & !is.null(shinytest2_dir)) {
+  if (!is.null(cypress_file) && !is.null(shinytest2_dir)) {
     message("Using the cypress file only")
     shinytest2_dir <- NULL
   }
@@ -34,7 +36,11 @@ performance_tests <- function(
 
   if (type == "cypress") {
     # creating the structure
-    project_path <- create_cypress_structure(app_dir = app_dir, port = port, debug = debug)
+    project_path <- create_cypress_structure(
+      app_dir = app_dir,
+      port = port,
+      debug = debug
+    )
 
     # copy the cypress test file from the current location and store it
     cypress_file_cp <- file.path(project_path, "cypress_tests.js")
@@ -45,7 +51,7 @@ performance_tests <- function(
       expr = {
         lapply(
           X = commit_list,
-          FUN = run_cypress_performance_test,
+          FUN = run_cypress_ptest,
           project_path = project_path,
           cypress_file = cypress_file_cp,
           debug = debug
@@ -77,7 +83,7 @@ performance_tests <- function(
       expr = {
         lapply(
           X = commit_list,
-          FUN = run_shinytest2_performance_test,
+          FUN = run_shinytest2_ptest,
           app_dir = app_dir,
           project_path = project_path,
           debug = debug
@@ -102,13 +108,19 @@ performance_tests <- function(
 #' @title Run the performance test based on a single commit using Cypress
 #'
 #' @param commit A commit hash code or a branch's name
-#' @param project_path The path to the project with all needed packages installed
-#' @param cypress_file The path to the .js file conteining cypress tests to be recorded
+#' @param project_path The path to the project with all needed packages
+#' installed
+#' @param cypress_file The path to the .js file conteining cypress tests to
+#' be recorded
 #' @param debug Logical. TRUE to display all the system messages on runtime
 #'
 #' @export
-run_cypress_performance_test <- function(commit, project_path, cypress_file, debug) {
-  files <- create_cypress_tests(project_path = project_path, cypress_file = cypress_file)
+run_cypress_ptest <- function(commit, project_path, cypress_file, debug) {
+  files <- create_cypress_tests(
+    project_path = project_path,
+    cypress_file = cypress_file
+  )
+
   js_file <- files$js_file
   txt_file <- files$txt_file
 
@@ -118,7 +130,10 @@ run_cypress_performance_test <- function(commit, project_path, cypress_file, deb
   message(glue("Switched to {commit}"))
 
   # run tests there
-  command <- glue("cd {project_path}; set -eu; exec yarn --cwd node performance-test")
+  command <- glue(
+    "cd {project_path}; ",
+    "set -eu; exec yarn --cwd node performance-test"
+  )
   system(command, ignore.stdout = !debug, ignore.stderr = !debug)
 
   # read the file saved by cypress
@@ -139,15 +154,17 @@ run_cypress_performance_test <- function(commit, project_path, cypress_file, deb
 #' @title Run the performance test based on a single commit using shinytest2
 #'
 #' @param commit A commit hash code or a branch's name
-#' @param project_path The path to the project with all needed packages installed
-#' @param cypress_file The path to the .js file conteining cypress tests to be recorded
+#' @param project_path The path to the project with all needed packages
+#' installed
+#' @param cypress_file The path to the .js file conteining cypress tests to
+#' be recorded
 #' @param txt_file The path to the file where it is aimed to save the times
 #' @param debug Logical. TRUE to display all the system messages on runtime
 #'
 #' @importFrom testthat ListReporter
 #' @importFrom shinytest2 test_app
 #' @export
-run_shinytest2_performance_test <- function(commit, app_dir, project_path, debug) {
+run_shinytest2_ptest <- function(commit, app_dir, project_path, debug) {
   # checkout to the desired commit
   checkout(branch = commit)
   date <- get_commit_date(branch = commit)
@@ -155,10 +172,20 @@ run_shinytest2_performance_test <- function(commit, app_dir, project_path, debug
 
   # run tests there
   my_reporter <- ListReporter$new()
-  test_app(app_dir = app_dir, reporter = my_reporter, stop_on_failure = FALSE, stop_on_warning = FALSE)
+  test_app(
+    app_dir = app_dir,
+    reporter = my_reporter,
+    stop_on_failure = FALSE,
+    stop_on_warning = FALSE
+  )
+
   perf_file <- as.data.frame(my_reporter$get_results())
   perf_file <- perf_file[, c("test", "real")]
-  perf_file$test <- gsub(x = perf_file$test, pattern = "\\{shinytest2\\} recording: ", replacement = "")
+  perf_file$test <- gsub(
+    x = perf_file$test,
+    pattern = "\\{shinytest2\\} recording: ",
+    replacement = ""
+  )
 
   perf_file <- cbind.data.frame(date = date, perf_file)
   colnames(perf_file) <- c("date", "test_name", "duration_ms")
