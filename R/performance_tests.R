174482d2ce94eb4dd0b0e9e6d59c7e03673f918f
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-19 12:20:08 +0200

    Allowing a vector and checking the vector length

--- R/performance_tests.R
+++ R/performance_tests.R
@@ -2,7 +2,9 @@
 #'
 #' @param commit_list A list of commit hash codes, branches' names or anything else you can use with git checkout [...]
 #' @param cypress_file The path to the .js file containing cypress tests to be recorded
+#' It can also be a vector of the same size of commit_list
 #' @param shinytest2_dir The directory with tests recorded by shinytest2
+#' It can also be a vector of the same size of commit_list
 #' @param app_dir The path to the application root
 #' @param port Port to run the app
 #' @param debug Logical. TRUE to display all the system messages on runtime
@@ -18,13 +20,26 @@ performance_tests <- function(
     port = 3333,
     debug = FALSE
 ) {
+  # Number of commits to test
+  n_commits <- length(commit_list)
+
   # Test whether we have everything we need
   if (is.null(cypress_file) & is.null(shinytest2_dir))
-    stop("You must provide a cypress_file or the shinytest2_dir")
+    stop("You must provide cypress_file or shinytest2_dir")
 
   if (!is.null(cypress_file) & !is.null(shinytest2_dir)) {
-    message("Using the cypress file only")
+    message("Using the cypress files only")
     shinytest2_dir <- NULL
+
+    if (length(cypress_file) == 1)
+      cypress_file <- rep(cypress_file, n_commits)
+    if (length(cypress_file) != n_commits)
+      stop("You must provide 1 or {n_commits} paths for cypress_file")
+  } else {
+    if (length(shinytest2_dir) == 1)
+      shinytest2_dir <- rep(shinytest2_dir, n_commits)
+    if (length(shinytest2_dir) != n_commits)
+      stop("You must provide 1 or {n_commits} paths for shinytest2_dir")
   }
 
   type <- ifelse(!is.null(cypress_file), "cypress", "shinytest2")
@@ -36,18 +51,14 @@ performance_tests <- function(
     # creating the structure
     project_path <- create_cypress_structure(app_dir = app_dir, port = port, debug = debug)
 
-    # copy the cypress test file from the current location and store it
-    cypress_file_cp <- file.path(project_path, "cypress_tests.js")
-    file.copy(from = cypress_file, to = cypress_file_cp)
-
     # apply the tests for each branch/commit
     perf_list <- tryCatch(
       expr = {
-        lapply(
-          X = commit_list,
+        mapply(
+          commit_list,
+          cypress_file,
           FUN = run_cypress_performance_test,
           project_path = project_path,
-          cypress_file = cypress_file_cp,
           debug = debug
         )
       },
@@ -108,6 +119,10 @@ performance_tests <- function(
 #'
 #' @export
 run_cypress_performance_test <- function(commit, project_path, cypress_file, debug) {
+  # copy the cypress test file from the current location and store it
+  cypress_file_cp <- file.path(project_path, "cypress_tests.js")
+  file.copy(from = cypress_file, to = cypress_file_cp)
+
   files <- create_cypress_tests(project_path = project_path, cypress_file = cypress_file)
   js_file <- files$js_file
   txt_file <- files$txt_file
