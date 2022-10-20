ac92113dcfc58b65bc4878e37f032a319849dde9
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-20 18:50:28 +0200

    Creating app.R in the tmp folder and adding fucntion to move tests to tmp folder

--- R/utils_shinytest2.R
+++ R/utils_shinytest2.R
@@ -1,14 +1,32 @@
 #' @title Create a temporary directory to store everything needed by shinytest2
 #'
-#' @param shinytest2_dir The path to the shinytest2 tests
-create_shinytest2_structure <- function(shinytest2_dir) {
+#' @param app_dir
+#'
+#' @importFrom glue glue
+create_shinytest2_structure <- function(app_dir) {
   # temp dir to run the tests
   dir_tests <- tempdir()
 
-  # copy everything to the temporary directory
-  system(glue("cp -r {shinytest2_dir} {dir_tests}"))
+  # shiny call
+  writeLines(
+    text = glue('shiny::runApp(appDir = "{app_dir}")'),
+    con = file.path(dir_tests, "app.R")
+  )
 
   # returning the project folder
   message(glue("Structure created at {dir_tests}"))
+
   return(dir_tests)
+}
+
+#' @title Move tests to a temporary folder
+#'
+#' @param project_path The path to the project
+#' @param shinytest2_dir The directory with tests recorded by shinytest2
+move_shinytest2_tests <- function(project_path, shinytest2_dir) {
+  # copy everything to the temporary directory
+  system(glue("cp -r {shinytest2_dir} {project_path}"))
+  tests_dir <- file.path(project_path, "tests")
+
+  return(tests_dir)
 }
