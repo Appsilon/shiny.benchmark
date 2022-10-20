1c8df0c5453b0c23957e591787ea03de1a1f3b4a
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-20 18:52:27 +0200

    Raising error when the length of input files is different than the commit list

--- R/performance_tests.R
+++ R/performance_tests.R
@@ -29,23 +29,17 @@ performance_tests <- function(
   if (!is.null(cypress_file) && !is.null(shinytest2_dir)) {
     message("Using the cypress file only")
     shinytest2_dir <- NULL
-
-    if (length(cypress_file) == 1)
-      cypress_file <- rep(cypress_file, n_commits)
-    if (length(cypress_file) != n_commits)
-      stop("You must provide 1 or {n_commits} paths for cypress_file")
-  } else {
-    if (length(shinytest2_dir) == 1)
-      shinytest2_dir <- rep(shinytest2_dir, n_commits)
-    if (length(shinytest2_dir) != n_commits)
-      stop("You must provide 1 or {n_commits} paths for shinytest2_dir")
   }
 
   type <- ifelse(!is.null(cypress_file), "cypress", "shinytest2")
+  obj_name <- ifelse(type == "cypress", "cypress_file", "shinytest2_dir")
 
-  # getting the current branch
-  current_branch <- get_commit_hash()
+  if (length(get(obj_name)) == 1)
+    assign(obj_name, rep(cypress_file, n_commits))
+  if (length(get(obj_name)) != n_commits)
+    stop("You must provide 1 or {n_commits} paths for {obj_name}")
 
+  # run tests
   if (type == "cypress") {
     perf_list <- ptest_cypress(
       commit_list = commit_list,
