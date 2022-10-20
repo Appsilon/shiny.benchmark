a7fd3d5b8e1018a4bea0c007ca4ded887c02979e
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-20 12:04:59 +0200

    Split cypress and shinytest2 functions into two files

--- R/performance_tests.R
+++ R/performance_tests.R
@@ -47,159 +47,21 @@ performance_tests <- function(
   current_branch <- get_commit_hash()
 
   if (type == "cypress") {
-    # creating the structure
-    project_path <- create_cypress_structure(
+    perf_list <- ptest_cypress(
+      commit_list = commit_list,
+      cypress_file = cypress_file,
       app_dir = app_dir,
       port = port,
       debug = debug
     )
-
-    # apply the tests for each branch/commit
-    perf_list <- tryCatch(
-      expr = {
-        mapply(
-          commit_list,
-          cypress_file,
-          FUN = run_cypress_performance_test,
-          project_path = project_path,
-          debug = debug
-        )
-      },
-      error = function(e) {
-        message(e)
-      },
-      finally = {
-        checkout(branch = current_branch)
-        message(glue("Switched back to {current_branch}"))
-
-        # Cleaning the temporary directory
-        unlink(
-          x = c(
-            file.path(project_path, "node"),
-            file.path(project_path, "tests")
-          ),
-          recursive = TRUE
-        )
-      }
-    )
   } else {
-    # creating the structure
-    project_path <- create_shinytest2_structure(shinytest2_dir = shinytest2_dir)
-
-    # apply the tests for each branch/commit
-    perf_list <- tryCatch(
-      expr = {
-        lapply(
-          X = commit_list,
-          FUN = run_shinytest2_ptest,
-          app_dir = app_dir,
-          project_path = project_path,
-          debug = debug
-        )
-      },
-      error = function(e) {
-        message(e)
-      },
-      finally = {
-        checkout(branch = current_branch)
-        message(glue("Switched back to {current_branch}"))
-
-        # Cleaning the temporary directory
-        unlink(x = file.path(project_path, "tests"), recursive = TRUE)
-      }
+    perf_list <- ptest_shinytest2(
+      commit_list,
+      shinytest2_dir,
+      app_dir,
+      debug
     )
   }
 
   return(perf_list)
-}
-
-#' @title Run the performance test based on a single commit using Cypress
-#'
-#' @param commit A commit hash code or a branch's name
-#' @param project_path The path to the project with all needed packages
-#' installed
-#' @param cypress_file The path to the .js file conteining cypress tests to
-#' be recorded
-#' @param debug Logical. TRUE to display all the system messages on runtime
-#'
-#' @importFrom utils read.table
-#' @export
-run_cypress_ptest <- function(commit, project_path, cypress_file, debug) {
-  files <- create_cypress_tests(
-    project_path = project_path,
-    cypress_file = cypress_file
-  )
-
-  js_file <- files$js_file
-  txt_file <- files$txt_file
-
-  # checkout to the desired commit
-  checkout(branch = commit)
-  date <- get_commit_date(branch = commit)
-  message(glue("Switched to {commit}"))
-
-  # run tests there
-  command <- glue(
-    "cd {project_path}; ",
-    "set -eu; exec yarn --cwd node performance-test"
-  )
-  system(command, ignore.stdout = !debug, ignore.stderr = !debug)
-
-  # read the file saved by cypress
-  perf_file <- read.table(file = txt_file, header = FALSE, sep = ";")
-  perf_file <- cbind.data.frame(date = date, perf_file)
-  colnames(perf_file) <- c("date", "test_name", "duration_ms")
-
-  # removing temp files
-  unlink(x = c(js_file, txt_file))
-
-  # removing anything new in the github repo
-  checkout_files()
-
-  # return times
-  return(perf_file)
-}
-
-#' @title Run the performance test based on a single commit using shinytest2
-#'
-#' @param commit A commit hash code or a branch's name
-#' @param app_dir The path to the application root
-#' @param project_path The path to the project with all needed
-#' packages installed
-#' @param debug Logical. TRUE to display all the system messages on runtime
-#'
-#' @importFrom testthat ListReporter
-#' @importFrom shinytest2 test_app
-#' @export
-run_shinytest2_ptest <- function(commit, app_dir, project_path, debug) {
-  # checkout to the desired commit
-  checkout(branch = commit)
-  date <- get_commit_date(branch = commit)
-  message(glue("Switched to {commit}"))
-
-  # run tests there
-  my_reporter <- ListReporter$new()
-  test_app(
-    app_dir = app_dir,
-    reporter = my_reporter,
-    stop_on_failure = FALSE,
-    stop_on_warning = FALSE
-  )
-
-  perf_file <- as.data.frame(my_reporter$get_results())
-  perf_file <- perf_file[, c("test", "real")]
-  perf_file$test <- gsub(
-    x = perf_file$test,
-    pattern = "\\{shinytest2\\} recording: ",
-    replacement = ""
-  )
-
-  perf_file <- cbind.data.frame(date = date, perf_file)
-  colnames(perf_file) <- c("date", "test_name", "duration_ms")
-
-  # removing anything new in the github repo
-  checkout_files()
-
-  # return times
-  return(perf_file)
 }
---
+++ R/tests_shinytest2.R
@@ -0,0 +1,83 @@
+#' @title Run the performance test based on a multiple commits using shinytest2
+#'
+#' @param commit_list A list of commit hash codes, branches' names or anything
+#' else you can use with git checkout [...]
+#' @param shinytest2_dir The directory with tests recorded by shinytest2
+#' It can also be a vector of the same size of commit_list
+#' @param app_dir The path to the application root
+#' @param debug Logical. TRUE to display all the system messages on runtime
+#'
+#' @export
+ptest_shinytest2 <- function(commit_list, shinytest2_dir, app_dir, debug) {
+  # creating the structure
+  project_path <- create_shinytest2_structure(shinytest2_dir = shinytest2_dir)
+
+  # apply the tests for each branch/commit
+  perf_list <- tryCatch(
+    expr = {
+      lapply(
+        X = commit_list,
+        FUN = run_shinytest2_ptest,
+        app_dir = app_dir,
+        project_path = project_path,
+        debug = debug
+      )
+    },
+    error = function(e) {
+      message(e)
+    },
+    finally = {
+      checkout(branch = current_branch)
+      message(glue("Switched back to {current_branch}"))
+
+      # Cleaning the temporary directory
+      unlink(x = file.path(project_path, "tests"), recursive = TRUE)
+    }
+  )
+
+  return(perf_list)
+}
+
+#' @title Run the performance test based on a single commit using shinytest2
+#'
+#' @param commit A commit hash code or a branch's name
+#' @param app_dir The path to the application root
+#' @param project_path The path to the project with all needed
+#' packages installed
+#' @param debug Logical. TRUE to display all the system messages on runtime
+#'
+#' @importFrom testthat ListReporter
+#' @importFrom shinytest2 test_app
+#' @export
+run_shinytest2_ptest <- function(commit, app_dir, project_path, debug) {
+  # checkout to the desired commit
+  checkout(branch = commit)
+  date <- get_commit_date(branch = commit)
+  message(glue("Switched to {commit}"))
+
+  # run tests there
+  my_reporter <- ListReporter$new()
+  test_app(
+    app_dir = app_dir,
+    reporter = my_reporter,
+    stop_on_failure = FALSE,
+    stop_on_warning = FALSE
+  )
+
+  perf_file <- as.data.frame(my_reporter$get_results())
+  perf_file <- perf_file[, c("test", "real")]
+  perf_file$test <- gsub(
+    x = perf_file$test,
+    pattern = "\\{shinytest2\\} recording: ",
+    replacement = ""
+  )
+
+  perf_file <- cbind.data.frame(date = date, perf_file)
+  colnames(perf_file) <- c("date", "test_name", "duration_ms")
+
+  # removing anything new in the github repo
+  checkout_files()
+
+  # return times
+  return(perf_file)
+}
