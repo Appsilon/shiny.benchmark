f33454a7ffd31f8bc24a8db0a44ddf64e14099a8
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-26 13:09:27 +0200

    Adding messages to identify the error

--- R/tests_cypress.R
+++ R/tests_cypress.R
@@ -102,10 +102,12 @@ run_cypress_ptest <- function(
   if (use_renv) restore_env(branch = commit, renv_prompt = renv_prompt)
 
   # get Cypress files
+  message(list.files(project_path))
   files <- create_cypress_tests(
     project_path = project_path,
     cypress_file = cypress_file
   )
+  message(list.files(project_path))
 
   js_file <- files$js_file
   txt_file <- files$txt_file
