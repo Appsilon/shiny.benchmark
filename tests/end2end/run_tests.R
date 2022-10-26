df451ab2019022b269ee8e7a2925bb1c7e87d9bf
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-26 16:58:21 +0200

    Updating tests based on the new arguments of the package

--- tests/end2end/run_tests.R
+++ tests/end2end/run_tests.R
@@ -10,33 +10,42 @@ library(shiny.performance)
 # commits to compare
 type <- args[[1]]
 commit_list <- args[[2]]
-tests <- args[[3]]
-use_renv <- args[[4]]
+dir <- args[[3]]
+pattern <- args[[4]]
+use_renv <- args[[5]]
+n_rep <- args[[6]]
 
 if (type == "cypress") {
   # run performance check using Cypress
   out <- shiny.performance::performance_tests(
     commit_list = commit_list,
-    cypress_file = tests,
+    cypress_dir = dir,
+    tests_pattern = pattern,
     app_dir = getwd(),
     use_renv = use_renv,
     renv_prompt = FALSE,
     port = 3333,
+    n_rep = n_rep,
     debug = FALSE
   )
 } else {
   # run performance check using shinytest2
   out <- shiny.performance::performance_tests(
     commit_list = commit_list,
-    shinytest2_dir = tests,
+    shinytest2_dir = dir,
+    tests_pattern = pattern,
     app_dir = getwd(),
     use_renv = use_renv,
     renv_prompt = FALSE,
     port = 3333,
+    n_rep = n_rep,
     debug = FALSE
   )
 }
 
 # checks
 stopifnot(length(out) == length(commit_list))
+stopifnot(length(out[[1]]) >= n_rep)
+
+# deactivate renv
 renv::deactivate()
