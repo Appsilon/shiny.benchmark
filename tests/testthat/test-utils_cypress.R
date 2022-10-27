bb78ed2c08bba14281bbf5e42987595a29c38453
DouglasMesquita <douglasrm.azevedo@gmail.com>
2022-10-27 13:25:17 +0200

    Improving cypress tests (lintr)

--- tests/testthat/test-utils_cypress.R
+++ tests/testthat/test-utils_cypress.R
@@ -10,28 +10,18 @@ test_that("Check if we are able to add Cypress code to a txt file", {
 
 test_that("Check if we are able to copy file content from a file to another", {
   tmp_dir <- tempdir()
-  tmp_file <- tempfile(tmpdir = tmp_dir, fileext = ".txt")
+  tmp_file <- tempfile(tmpdir = tmp_dir, fileext = ".js")
   content_before <- "TEST"
   writeLines(text = content_before, con = tmp_file)
 
-  dir.create(file.path(tmp_dir, "tests", "cypress", "integration"), showWarnings = FALSE, recursive = TRUE)
-  files <- create_cypress_tests(project_path = tmp_dir, cypress_file = tmp_file)
+  integration_dir <- file.path(tmp_dir, "tests", "cypress", "integration")
+  dir.create(integration_dir, showWarnings = FALSE, recursive = TRUE)
+  files <- create_cypress_tests(
+    project_path = tmp_dir,
+    cypress_dir = tmp_dir,
+    tests_pattern = ".js"
+  )
   content_after <- readLines(con = files$js_file, n = 1)
 
   expect_true(content_after == content_before)
 })
-
-test_that("Check whether we have are able to create Cypress structure correctly or not", {
-  tmp_dir <- create_cypress_structure(
-    app_dir = getwd(),
-    port = 3333,
-    debug = FALSE
-  )
-
-  expect_true(file.exists(file.path(tmp_dir, "node")))
-  expect_true(file.exists(file.path(tmp_dir, "node", "root")))
-  expect_true(file.exists(file.path(tmp_dir, "tests")))
-  expect_true(file.exists(file.path(tmp_dir, "tests", "cypress", "plugins")))
-})
-
-
