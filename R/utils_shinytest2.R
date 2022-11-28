7c045392c2d6300ed6c8e2f6b92d4fa38cef4048
André Veríssimo <afsverissimo@gmail.com>
2022-11-28 14:39:29 +0100

    revert move_shinytest2_tests changes

--- R/utils_shinytest2.R
+++ R/utils_shinytest2.R
@@ -28,22 +28,9 @@ create_shinytest2_structure <- function(app_dir) {
 #'
 #' @keywords internal
 move_shinytest2_tests <- function(project_path, shinytest2_dir) {
-  tests_dir <- fs::path(project_path, "tests")
   # copy everything to the temporary directory
-  if (fs::is_dir(shinytest2_dir)) {
-    fs::dir_copy(
-      path = shinytest2_dir,
-      new_path = tests_dir,
-      overwrite = TRUE
-    )
-  } else {
-    # should never reach this
-    fs::file_copy(
-      path = shinytest2_dir,
-      new_path = tests_dir,
-      overwrite = TRUE
-    )
-  }
+  file.copy(from = shinytest2_dir, to = project_path, recursive = TRUE)
+  tests_dir <- file.path(project_path, "tests")
 
   return(tests_dir)
 }
--- tests/testthat/test-utils_shinytest2.R
+++ tests/testthat/test-utils_shinytest2.R
@@ -1,50 +1,21 @@
-library(fs)
-
 test_that("Check if we are able to move files properly", {
   tmp_dir <- tempdir()
 
-  # Create the folder that contains the tests, it's named tst
-  # on purpuse as it won't be a valid directory and shinytests2 would
-  # fail
   tmp_dir1 <- file.path(tmp_dir, "folder1")
+  dir.create(tmp_dir1, showWarnings = FALSE)
+  tmp_dir2 <- file.path(tmp_dir, "folder2")
+  dir.create(tmp_dir2, showWarnings = FALSE)
 
-  dir.create(path = tmp_dir1, recursive = TRUE)
-  shinytest2_dir <- fs::path(tmp_dir1, "tst")
-  dir.create(shinytest2_dir, showWarnings = FALSE)
-  file.create(fs::path(shinytest2_dir, "some_example.txt"))
-  file.create(fs::path(shinytest2_dir, "some_example2.txt"))
-  file.create(fs::path(shinytest2_dir, "some_example3.txt"))
+  shinytest2_dir <- file.path(tmp_dir1, "tst")
+  shinytest2_dir_copy <- file.path(tmp_dir2, "tst")
+  dir.create(path = shinytest2_dir, showWarnings = FALSE)
 
-  # Create a mock project path base directory
-  tmp_dir2 <- fs::path(tmp_dir, "folder2")
-  dir.create(tmp_dir2, recursive = TRUE)
-  project_path <- tmp_dir2
-  dir.create(fs::path(tmp_dir2, "something"))
-  file.create(fs::path(shinytest2_dir, "root.txt"))
+  move_shinytest2_tests(project_path = tmp_dir2, shinytest2_dir = shinytest2_dir)
 
-  # The result of the copy should land on tests folder
-  shinytest2_dir_copy_manual <- file.path(tmp_dir2, "tests")
-
-  # [ACTION] Actual copy of the shinytest2 files
-  shinytest2_dir_copy_auto <- move_shinytest2_tests(
-    project_path = tmp_dir2,
-    shinytest2_dir = shinytest2_dir
-  )
-
-  # Test if the copy was successful
-  expect_true(dir.exists(shinytest2_dir_copy_manual))
-  expect_true(dir.exists(shinytest2_dir_copy_auto))
-  expect_equal(shinytest2_dir_copy_auto, shinytest2_dir_copy_manual)
-
-  short_var <- shinytest2_dir_copy_auto
-  str_mask <- "some_example{input}.txt"
-  c("", 2, 3) |>
-    sapply(function(input) {
-      expect_true(file.exists(fs::path(short_var, glue(str_mask))))
-    })
+  expect_true(file.exists(shinytest2_dir_copy))
 })
 
 test_that("Check if we are able to create shinytest2 structure", {
   tmp_dir <- create_shinytest2_structure(app_dir = ".")
-  expect_true(fs::file_exists(fs::path(tmp_dir, "app.R")))
+  expect_true(file.exists(file.path(tmp_dir, "app.R")))
 })
