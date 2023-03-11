cdf5af141d1fab035808915d457503c776a54c38
douglas <douglasrm.azevedo@gmail.com>
2023-03-11 15:30:27 +0100

    Updating tutorial and examples to make sure it works as expected

--- inst/examples/app/server.R
+++ inst/examples/app/server.R
@@ -1,24 +1,24 @@
+times <- c(10, 5, 2)
+
 function(input, output, session) {
   # Sys.sleep
   react1 <- eventReactive(input$run1, {
     out <- system.time(
-      Sys.sleep(1 + rexp(n = 1, rate = 10))
+      Sys.sleep(times[1] + rexp(n = 1, rate = 1))
     )
 
     return(out[3])
   })
-
   react2 <- eventReactive(input$run2, {
     out <- system.time(
-      Sys.sleep(0.5 + rexp(n = 1, rate = 10))
+      Sys.sleep(times[2] + rexp(n = 1, rate = 1))
     )
 
     return(out[3])
   })
-
   react3 <- eventReactive(input$run3, {
     out <- system.time(
-      Sys.sleep(0.1 + rexp(n = 1, rate = 10))
+      Sys.sleep(times[3] + rexp(n = 1, rate = 1))
     )
 
     return(out[1])
@@ -26,14 +26,12 @@ function(input, output, session) {
 
   # outputs
   output$out1 <- renderUI({
-    tags$span(round(react1()), style = "font-size: 500px;")
+    tags$span(round(react1()), style = "font-size: 5vw;")
   })
-
   output$out2 <- renderUI({
-    tags$span(round(react2()), style = "font-size: 500px;")
+    tags$span(round(react2()), style = "font-size: 5vw;")
   })
-
   output$out3 <- renderUI({
-    tags$span(round(react3()), style = "font-size: 500px;")
+    tags$span(round(react3()), style = "font-size: 5vw;")
   })
 }
